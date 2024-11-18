7de62c7900350ad5c4dea78fbba25dd4b38d2f8e
Glitch (muddy-desert-visage) <none>
2024-11-18 02:05:16 +0000

    👟🍽 Checkpoint ./js/heddohon.js:58362660/6265 ./templates/heddohon.html:58362660/9662 ./templates/app.html:58362660/1732 ./templates/more.html:58362660/1731 ./templates/tool.html:58362660/1731

--- js/heddohon.js
+++ js/heddohon.js
@@ -21,29 +21,3 @@ bgmButton.addEventListener("click", () => {
     bgmButton.textContent = "🎵 bgmを再生する 🎵";
   }
 });
-
-const darkModeToggle = document.getElementById("dark-mode-toggle");
-
-darkModeToggle.addEventListener("click", () => {
-  const isDarkMode = document.body.classList.toggle("dark-mode");
-  setCookie("darkMode", isDarkMode, 7);
-});
-
-function setCookie(name, value, days) {
-  const expires = new Date();
-  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
-  document.cookie =
-    name + "=" + value + ";expires=" + expires.toUTCString() + ";path=/";
-}
-
-function getCookie(name) {
-  const nameEQ = name + "=";
-  const ca = document.cookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i].trim();
-    if (c.indexOf(nameEQ) === 0) {
-      return c.substring(nameEQ.length, c.length);
-    }
-  }
-  return null;
-}
